84d67f08c2e578eafdb38b97ca82a533f0195528
grad0ff <grad0ff@mail.ru>
2022-08-08 21:50:57 +0300

    added second test, added owner

--- src/test/java/config/Credentials.java
+++ src/test/java/config/Credentials.java
@@ -3,19 +3,14 @@ package config;
 import org.aeonbits.owner.Config;
 
 
-@Config.Sources("classpath: config/credentials.properties")
+@Config.Sources("classpath:config/credentials.properties")
 public interface Credentials extends Config {
 
 	@Key("login")
 	String getLogin();
 
-
 	@Key("password")
 	String getPassword();
-
-
-	@Key("remoteHost")
-	String getRemoteHost();
 
 	@Key("appUrl")
 	String getAppUrl();
